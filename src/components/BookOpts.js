ddd824a1dd2b28925450661a1c5c5803780b80b8
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-28 20:39:36 +0200

    update BookOpts logic

--- src/components/BookOpts.js
+++ src/components/BookOpts.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
-const BookOpts = () => (
-  <div className="book-options">
-    <a href="/#">Comments</a>
-    {' | '}
-    <a href="/#">Remove</a>
-    {' | '}
-    <a href="/#">Edit</a>
-  </div>
-);
+import { removeBook } from '../redux/books/books';
+
+const BookOpts = ({ id }) => {
+  const dispatch = useDispatch();
+
+  const RemoveBook = () => {
+    dispatch(removeBook({ id }));
+  };
+
+  return (
+    <div className="book-options">
+      <a href="/#">Comments</a>
+      {' | '}
+      <a href="/#" onClick={RemoveBook}>Remove</a>
+      {' | '}
+      <a href="/#">Edit</a>
+    </div>
+  );
+};
+
+BookOpts.propTypes = ({
+  id: PropTypes.number,
+}).isRequired;
 
 export default BookOpts;
