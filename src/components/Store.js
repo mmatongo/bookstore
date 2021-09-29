fb825fe6854d974da6ecb470f2e29dd8bcf793be
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-29 12:12:52 +0200

    update store.js to fetch book data and push it to bookStore

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from './Book';
+import pullBooks from './Slice';
 
 const Store = () => {
-  const bookStore = useSelector((store) => store.booksReducer);
-  const bookComponent = bookStore.map(
-    (book) => (
-      <Book
-        key={book.id}
-        id={book.id}
-        author={book.author}
-        title={book.title}
-        category={book.category}
-      />
-    ),
-  );
+  const bookStore = [];
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(pullBooks());
+  }, []);
+
+  let storeFetch = useSelector((store) => store.booksReducer.books);
+  if (storeFetch.error) {
+    storeFetch = [];
+  } else {
+    const bookIDs = Object.keys(storeFetch);
+    bookIDs.forEach((id) => {
+      storeFetch[id].map((book) => (
+        bookStore.push(
+          <Book
+            key={id}
+            id={id}
+            title={book.title}
+            category={book.category}
+          />,
+        )
+      ));
+    });
+  }
 
   return (
     <>
-      { bookComponent }
+      { bookStore }
     </>
   );
 };
