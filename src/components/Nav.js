736bcbb8a03894757915eedfa70fc59cd0185424
Daniel M. Matongo <m_matongo@icloud.com>
2021-09-29 15:30:31 +0200

    update nav classes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,24 +2,33 @@ import React from 'react';
 import {
   Link,
 } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => (
-  <>
+  <div className="header">
     <div className="nav-title">
-      <h1 className="nav-main">Bookstore CMS</h1>
-      <nav className="nav-links">
-        <ul>
-          <li>
-            <Link to="/">BOOKS</Link>
-          </li>
-          <li>
-            <Link to="/categories">CATEGORIES</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="nav">
+        <h1 className="nav-main">Bookstore CMS</h1>
+        <nav className="nav-links">
+          <ul>
+            <li>
+              <Link to="/">BOOKS</Link>
+            </li>
+            <li>
+              <Link to="/categories">CATEGORIES</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="avatar">
+        <span className="avatar-circle">
+          <FontAwesomeIcon icon={faUserAlt} size="lg" color="#0290ff" />
+        </span>
+      </div>
     </div>
-    <span><i /></span>
-  </>
+  </div>
 );
 
 export default Header;
